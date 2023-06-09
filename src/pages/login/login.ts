b5afceadee16675c9eabf127cb11080a508dbb5b
Ivan Consalter <ivanconsalter1@gmail.com>
2023-06-09 10:13:12 -0300

    disabled menu on login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, MenuController, NavController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,11 +8,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    private menuController: MenuController
+  ) { }
+
+  ionViewWillEnter() {
+    this.menuController.swipeEnable(false);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  ionViewDidLeave() {
+    this.menuController.swipeEnable(true);
+  }
+
+  login() {
+    this.navCtrl.setRoot('HomePage');
   }
 
 }
